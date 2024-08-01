fe85057f6e65219222d1c89d5e6515dd9c3ae949
Rose Northey <rose.northey@octopus.com>
2024-08-02 10:11:32 +1200

    push and pop for arrays done

--- Assignment1/queue.spec.ts
+++ Assignment1/queue.spec.ts
@@ -9,28 +9,7 @@ describe('test queue', () => {
     queue1.Push(otherNumber);
     queue1.Push(otherNumber);
     queue1.Pop();
-    queue1.Pop();
     const actual = queue1.data;
-    queue1.Push(8);
     expect(actual).not.toContain(firstNumber);
   });
-  //   test('correctly calculates Max of 3 added numbers', () => {
-  //     const queue1 = new queue();
-  //     queue1.Push(1);
-  //     queue1.Push(3);
-  //     queue1.Push(2);
-  //     const actual = queue1.Max();
-  //     const expected = 3;
-  //     expect(actual).toEqual(expected);
-  //   });
-  //   test('correctly calculates Max when high number is added then removed', () => {
-  //     const queue1 = new queue();
-  //     queue1.Push(30);
-  //     queue1.Push(1);
-  //     queue1.Push(1);
-  //     queue1.Pop();
-  //     const actual = queue1.Max();
-  //     const expected = 1;
-  //     expect(actual).toEqual(expected);
-  //   });
 });
--- Assignment1/queue.ts
+++ Assignment1/queue.ts
@@ -1,8 +1,7 @@
 export default class queue {
-  data: {[key: number]: number};
+  data: (number | undefined)[];
   iFront: number;
   iBack: number;
-
   constructor() {
     this.data = [];
     this.iFront = 0;
@@ -11,21 +10,46 @@ export default class queue {
   Push(newNumber: number) {
     this.data[this.iBack] = newNumber;
     this.iBack++;
-    console.log(this.data);
   }
   Pop() {
-    delete this.data[this.iFront];
+    this.data[this.iFront] = undefined;
     this.iFront++;
   }
-
-  // Max() {
-  // }
-  // [1 2 5 7 2 9 2 6 3 0 6]
-  // [1 2 5 7 2 9 2 6 3 0 6] -> 9
-  // [2 5 7 2 9 2 6 3 0 6] -> 9
-  // [2 6 3 0 6] -> 6
-  // [2 6 3 0 6 8] -> 8
 }
+// maxLength = 3
+// queues don't need to be reusable
+[1, 2, 3];
+
+// export default class queue {
+//   data: {[key: number]: number};
+//   iFront: number;
+//   iBack: number;
+//   // max: {[key: number]: number};
+
+//   constructor() {
+//     this.data = {};
+//     this.iFront = 0;
+//     this.iBack = 0;
+//     // this.max = {};
+//   }
+//   Push(newNumber: number) {
+//     this.data[this.iBack] = newNumber;
+//     this.iBack++;
+//   }
+//   Pop() {
+//     delete this.data[this.iFront];
+//     this.iFront++;
+//   }
+// }
+
+// Max() {
+// }
+// [1 2 5 7 2 9 2 6 3 0 6] -> 9
+// [1 2 5 7 7 9 9 9 9 9 9]
+
+// [2 5 7 2 9 2 6 3 0 6] -> 9
+// [2 6 3 0 6] -> 6
+// [2 6 3 0 6 8] -> 8
 
 // there are 2 types of changes to the max number
 // 1. changes to front of queue
