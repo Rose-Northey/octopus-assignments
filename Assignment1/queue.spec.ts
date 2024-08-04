88300631da10000ad42d537716b9b46b4642f3eb
Rose Northey <rose.northey@octopus.com>
2024-08-05 10:10:53 +1200

    WIP queue

--- Assignment1/queue.spec.ts
+++ Assignment1/queue.spec.ts
@@ -8,8 +8,18 @@ describe('test queue', () => {
     queue1.Push(firstNumber);
     queue1.Push(otherNumber);
     queue1.Push(otherNumber);
+    const returnedValue = queue1.Pop();
+    const actual = queue1.ToList();
+    expect(returnedValue).toEqual(firstNumber);
+  });
+  test('error is thrown when pop comes before push', () => {
+    const queue1 = new queue();
+    expect(() => queue1.Pop()).toThrow();
+  });
+  test('error is thrown when nothing left to pop', () => {
+    const queue1 = new queue();
+    queue1.Push(1);
     queue1.Pop();
-    const actual = queue1.data;
-    expect(actual).not.toContain(firstNumber);
+    expect(() => queue1.Pop()).toThrow();
   });
 });
--- Assignment1/queue.ts
+++ Assignment1/queue.ts
@@ -1,57 +1,58 @@
 export default class queue {
-  data: (number | undefined)[];
+  private data: number[];
   iFront: number;
   iBack: number;
+  iMax: number;
+  // orderedData: number[] you won't know where to put numbers which fall in between maximums
+
   constructor() {
     this.data = [];
     this.iFront = 0;
     this.iBack = 0;
+    this.iMax = 0;
   }
   Push(newNumber: number) {
     this.data[this.iBack] = newNumber;
     this.iBack++;
   }
   Pop() {
-    this.data[this.iFront] = undefined;
+    if (this.iFront === this.iBack) {
+      throw new Error('Nothing to pop!');
+    }
+    const returnedValue = this.data[this.iFront];
     this.iFront++;
+    return returnedValue;
   }
+  ToList() {
+    return this.data;
+  }
+  Max() {}
 }
-// maxLength = 3
-// queues don't need to be reusable
-[1, 2, 3];
 
-// export default class queue {
-//   data: {[key: number]: number};
-//   iFront: number;
-//   iBack: number;
-//   // max: {[key: number]: number};
+// this.data[this.iFront] = undefined;
+// INDEX LOOKUP IS constant Big O notation!!
+// Maximum array but with indexes?
+// e.g. [6, 3, 5, 2]
+//
+//store the index of the maximum numbers NOT the actual number
+// maximumIndexes = 0
+// -> for each number you add, compare it to the index at the maximum index point
+// when you add one to the end, imagine that you are
+// 2nd max index
+// [6, 5, 5, 2]
+// AND if you had [6, 3, 5, 2, 7] -> [7, 7, 7, 7, 7]
+// HOW DO I MAKE THIS?
+// [6] -> [6]
+// [6, 3] -> [6, 3]
+// [6, 3, 5] -> [6, 5, 5] change previous to 5
+// [6, 3, 5, 2] -> [6, 5, 5, 2]
+// [6, 3, 5, 2, 7] -> change all to 7 [7,7,7,7,7] DOES EXPAND WITH N
 
-//   constructor() {
-//     this.data = {};
-//     this.iFront = 0;
-//     this.iBack = 0;
-//     // this.max = {};
-//   }
-//   Push(newNumber: number) {
-//     this.data[this.iBack] = newNumber;
-//     this.iBack++;
-//   }
-//   Pop() {
-//     delete this.data[this.iFront];
-//     this.iFront++;
-//   }
-// }
+// If I have a maximum index array I would want it to be
+// [0 2 2 3]
+// so if nothing is popped, index points to 6, if 6 is popped index points to 5, if 5 is popped, index points to 2
 
-// Max() {
-// }
-// [1 2 5 7 2 9 2 6 3 0 6] -> 9
-// [1 2 5 7 7 9 9 9 9 9 9]
-
-// [2 5 7 2 9 2 6 3 0 6] -> 9
-// [2 6 3 0 6] -> 6
-// [2 6 3 0 6 8] -> 8
-
-// there are 2 types of changes to the max number
-// 1. changes to front of queue
-// 2. changes to back of the queue
-// -------> Any number could be the maximum given those two changes
+// HOW DO I MAKE THIS
+// [6] -> [0]
+// [6, 3] -> [0, 1]
+// [6, 3, 5] -> I check out what is at 1 and compare it to my new number IF its bigger, then
