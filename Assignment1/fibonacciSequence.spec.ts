fd3ae9bf1fb2ff300039a9b3c61b0e88bec37354
Rose Northey <rose.northey@octopus.com>
2024-08-22 10:15:35 +1200

    solve for stored memoization

--- Assignment1/fibonacciSequence.spec.ts
+++ Assignment1/fibonacciSequence.spec.ts
@@ -1,6 +1,6 @@
-import {FibonacciBottomUp} from './fibonacciSequence';
+import {FibonacciBottomUp, FibonacciStore} from './fibonacciSequence';
 
-describe('print Fibonacci Sequence given n', () => {
+describe('print Fibonacci Sequence given n using BottomUp', () => {
   test('print Fibonacci Sequence for n=0', () => {
     const myFibonacci = new FibonacciBottomUp();
     const sequenceIndex = 0;
@@ -27,6 +27,38 @@ describe('print Fibonacci Sequence given n', () => {
   });
   test('print Fibonacci Sequence for n=80', () => {
     const myFibonacci = new FibonacciBottomUp();
+    const sequenceIndex = 80;
+    const numberAtIndex = myFibonacci.getNumberAtIndex(sequenceIndex);
+    expect(numberAtIndex).toEqual(23416728348467685);
+  });
+});
+describe('print Fibonacci Sequence given n using a store', () => {
+  test('print Fibonacci Sequence for n=0', () => {
+    const myFibonacci = new FibonacciStore();
+    const sequenceIndex = 0;
+    const numberAtIndex = myFibonacci.getNumberAtIndex(sequenceIndex);
+    expect(numberAtIndex).toEqual(0);
+  });
+  test('print Fibonacci Sequence for n=1', () => {
+    const myFibonacci = new FibonacciStore();
+    const sequenceIndex = 1;
+    const numberAtIndex = myFibonacci.getNumberAtIndex(sequenceIndex);
+    expect(numberAtIndex).toEqual(1);
+  });
+  test('print Fibonacci Sequence for n=2', () => {
+    const myFibonacci = new FibonacciStore();
+    const sequenceIndex = 2;
+    const numberAtIndex = myFibonacci.getNumberAtIndex(sequenceIndex);
+    expect(numberAtIndex).toEqual(1);
+  });
+  test('print Fibonacci Sequence for n=10', () => {
+    const myFibonacci = new FibonacciStore();
+    const sequenceIndex = 10;
+    const numberAtIndex = myFibonacci.getNumberAtIndex(sequenceIndex);
+    expect(numberAtIndex).toEqual(55);
+  });
+  test('print Fibonacci Sequence for n=80', () => {
+    const myFibonacci = new FibonacciStore();
     const sequenceIndex = 80;
     const numberAtIndex = myFibonacci.getNumberAtIndex(sequenceIndex);
     expect(numberAtIndex).toEqual(23416728348467685);
--- Assignment1/fibonacciSequence.ts
+++ Assignment1/fibonacciSequence.ts
@@ -1,22 +1,33 @@
 export class FibonacciBottomUp {
   cachedSequence: number[];
-  topNumber: number;
-  beforeTopNumber: number;
   constructor() {
     this.cachedSequence = [0, 1];
-    this.topNumber = this.cachedSequence[1];
-    this.beforeTopNumber = this.cachedSequence[0];
   }
   public getNumberAtIndex(desiredIndex: number): number {
     if (this.cachedSequence.length > desiredIndex) {
       return this.cachedSequence[desiredIndex];
     } else {
-      this.topNumber = this.topNumber + this.beforeTopNumber;
-      this.beforeTopNumber = this.topNumber - this.beforeTopNumber;
-      this.cachedSequence.push(this.topNumber);
+      this.cachedSequence.push(
+        this.cachedSequence[this.cachedSequence.length - 1] +
+          this.cachedSequence[this.cachedSequence.length - 2]
+      );
       return this.getNumberAtIndex(desiredIndex);
     }
   }
 }
 
-// bottom up approach done initially
+export class FibonacciStore {
+  cachedSequence: number[];
+  constructor() {
+    this.cachedSequence = [0, 1];
+  }
+  public getNumberAtIndex(desiredIndex: number): number {
+    if (this.cachedSequence[desiredIndex] === undefined) {
+      this.cachedSequence.push(
+        this.getNumberAtIndex(desiredIndex - 1) +
+          this.getNumberAtIndex(desiredIndex - 2)
+      );
+    }
+    return this.cachedSequence[desiredIndex];
+  }
+}
